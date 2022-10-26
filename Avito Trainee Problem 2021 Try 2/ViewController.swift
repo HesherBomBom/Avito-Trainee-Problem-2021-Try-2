759073f6411859123dfc213ce0842a320bde21fd
pzykov <zpavel411@gmail.com>
2022-10-26 16:54:03 +0300

    Initial Commit

---
+++ Avito Trainee Problem 2021 Try 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avito Trainee Problem 2021 Try 2
+//
+//  Created by Pavel Zykov on 26.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
